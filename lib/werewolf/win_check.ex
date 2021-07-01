f2ed4b64c20d4aa8dceb30255a734f0c924d310e
Thomas Blakey <tomblakey89@googlemail.com>
2021-07-02 00:00:13 +0100

    fix team bug

--- lib/werewolf/win_check.ex
+++ lib/werewolf/win_check.ex
@@ -1,6 +1,13 @@
 defmodule Werewolf.WinCheck do
   alias Werewolf.Player
 
+  @team_conversion %{
+    werewolf_aux: :villager,
+    werewolf: :werewolf,
+    fool: :villager,
+    villager: :villager
+  }
+
   def by_remaining_players(:fool_win, _players), do: {:ok, :fool_win}
 
   def by_remaining_players(_existing_win_status, players) do
@@ -18,7 +25,7 @@ defmodule Werewolf.WinCheck do
   defp by_team(players) do
     Enum.filter(players, fn {_, player} -> player.alive end)
     |> Enum.reduce(%{villager: 0, werewolf: 0}, fn {_key, %{role: role}}, acc ->
-      Map.update!(acc, Player.player_team(role), &(&1 + 1))
+      Map.update!(acc, @team_conversion[Player.player_team(role)], &(&1 + 1))
     end)
   end
 end
